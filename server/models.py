23ae7f1aff94a4e60d2238a2102e003ebfcc28d3
samuelgitau <amsamgitau@gmail.com>
2024-10-02 08:07:15 +0300

    complete lab

--- server/models.py
+++ server/models.py
@@ -1,4 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, make_response
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
@@ -7,7 +7,7 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 
-class Pet(db.Model):
+class Pet(db.Model, SerializerMixin):
     __tablename__ = 'pets'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -16,3 +16,29 @@ class Pet(db.Model):
 
     def __repr__(self):
         return f'<Pet {self.id}, {self.name}, {self.species}>'
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+
+    if pet:
+        body = pet.to_dict()
+        status=200
+    else:
+        body = {'message': f'Pet {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
+
+def pet_by_species(species):
+    pets=[]
+    for pet in Pet.query.filter_by(species=species).all():
+        pets.append(pet.to_dict())
+    body={
+        'count':len(pets),
+        'pets':pets
+    } 
+    
+    return make_response(body, 200)   
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
